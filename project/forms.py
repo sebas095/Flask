5782b8b88dca618f547fc5bd7c1e54fd451f1fb5
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-09-19 00:38:06 -0500

    validators

--- project/forms.py
+++ project/forms.py
@@ -1,8 +1,30 @@
 from wtforms import Form
 from wtforms import StringField, TextField
 from wtforms.fields.html5 import EmailField
+from wtforms import HiddenField
+from wtforms import validators
+
+def length_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('El campo debe estar vacio.')
 
 class CommentForm(Form):
-    username = StringField('Username')
-    email = EmailField('Correo electronico')
-    comment = TextField('Comentario')
+    username = StringField('Username',
+            [
+              validators.Required(message = 'El username es requerido'),
+              validators.length(min = 4, max = 25, message = 'Ingrese un username valido!')
+            ])
+
+    email = EmailField('Correo electronico',
+            [
+              validators.Required(message = 'El email es requerido'),
+              validators.Email(message = 'Ingrese un email valido')
+            ])
+
+    comment = TextField('Comentario',
+            [
+              validators.Required(message = 'El comentario es requerido'),
+              validators.length(min = 4, max = 25, message = 'Ingrese un coomentario valido!')
+            ])
+
+    honeypot = HiddenField('', [length_honeypot])
--- project/main.py
+++ project/main.py
@@ -2,14 +2,23 @@
 ## -*- coding: utf-8 -*-
 from flask import Flask
 from flask import render_template
+from flask import request
 
 import forms
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    comment_form = forms.CommentForm()
+    comment_form = forms.CommentForm(request.form)
+
+    if request.method == 'POST' and comment_form.validate():
+        print comment_form.username.data
+        print comment_form.email.data
+        print comment_form.comment.data
+    else:
+        print 'Error en el formulario'
+
     title = "Curso Flask"
     return render_template('index.html', title = title, form = comment_form)
 
